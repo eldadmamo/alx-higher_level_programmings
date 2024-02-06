bca8f49853e0c4561686fb1f0edccfeba635840e
Eldad <eldadf456@gmail.com>
2024-02-06 12:55:50 -0800

    0x0B-python-input_output

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Reads from standard input and computes metrics
+"""Reads from standard input and computes metrics.
 
 After every ten lines or the input of a keyboard interruption (CTRL + C),
 prints the following statistics:
@@ -9,7 +9,7 @@ prints the following statistics:
 
 
 def print_stats(size, status_codes):
-    """return accumulated metrics.
+    """Print accumulated metrics.
 
     Args:
         size (int): accumulated read file size.
