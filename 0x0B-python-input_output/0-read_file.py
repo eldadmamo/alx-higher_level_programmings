531e12b6a631fd1b9518057bada99b709f06f26a
Eldad <eldadf456@gmail.com>
2024-02-06 11:32:21 -0800

    0x0B-python-input_output

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,23 +1,8 @@
 #!/usr/bin/python3
-"""file reading"""
+"""Defines a text file-reading function."""
 
 
 def read_file(filename=""):
-    """
-    Reads the contents file and prints.
-
-    Args:
-        filename (str, optional): The name of the file 
-        read. If no filename is provided, an empty string 
-
-    Returns:
-        None
-
-    Example:
-        read_file("example.txt")
-
-    This code read contents of the file named "example.txt"
-    and print .
-    """
-    with open(filename, encoding="utf-8") as file:
-        print(file.read(), end="")
+    """Print the contents of a UTF8 text file to stdout."""
+    with open(filename, encoding="utf-8") as f:
+        print(f.read(), end="")
