a3250a504eebb86da6279d84f9f8a787903b3bb9
Eldad <eldadf456@gmail.com>
2024-02-06 11:34:50 -0800

    0x0B-python-input_output

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,20 +1,15 @@
 #!/usr/bin/python3
-"""A script that add all arg in a pythin list
-and save them in a file"""
+"""Add all arguments to a Python and save file."""
+import sys
 
-from sys import argv
+if __name__ == "__main__":
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = \
+        __import__('6-load_from_json_file').load_from_json_file
 
-save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
-load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
-
-filename = "add_item.json"
-
-try:
-    json_list = load_from_json_file(filename)
-except FileNotFoundError:
-    json_list = []
-
-for arg in argv[1:]:
-    json_list.append(arg)
-
-save_to_json_file(json_list, filename)
+    try:
+        items = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        items = []
+    items.extend(sys.argv[1:])
+    save_to_json_file(items, "add_item.json")
