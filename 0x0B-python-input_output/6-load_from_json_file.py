f5d930b072f606883ec9a4d44c329da3b01fd9ca
Eldad <eldadf456@gmail.com>
2024-02-06 11:36:53 -0800

    0x0B-python-input_output

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,22 +1,9 @@
 #!/usr/bin/python3
-"""Object from a file"""
-
+"""Defines a JSON file-reading function."""
 import json
 
 
-def load_from_json_file(filename: str) -> dict:
-    """
-    Load JSON file
-
-    Args:
-        filename (str): The name of the JSON file.
-
-    Returns:
-        dict: loaded JSON object.
-
-    Raises:
-        FileNotFoundError: If specified file does not occur.
-        json.JSONDecodeError: file contents are not valid JSON.
-    """
-    with open(filename, "r") as file:
-        return json.load(file)
+def load_from_json_file(filename):
+    """Create a Python object from a JSON file."""
+    with open(filename) as f:
+        return json.load(f)
