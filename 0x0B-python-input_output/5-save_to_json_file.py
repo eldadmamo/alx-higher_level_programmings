665a3f9cf1ac02852da799db0894df3504052b57
Eldad <eldadf456@gmail.com>
2024-02-06 10:55:55 -0800

    0x0B-python-input_output

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""object to a Save"""
+"""object to a text JSON"""
 
 import json
 
@@ -9,22 +9,22 @@ def save_to_json_file(my_obj, filename):
     Save an object JSON file.
 
     Args:
-        my_obj (any): The object to be save JSON file.
-        filename (str): The name of the file to the object 
+        my_obj (any): The object to the JSON file.
+        filename (str): The name of the file.
 
     Returns:
         None
 
     Example:
-        # Saving a dictionary JSON file
+        # Saving a dictionary to a JSON file
         data = {"name": "John", "age": 30}
         save_to_json_file(data, "output.json")
-        # The contents of data to output.json in JSON format
+        # The contents of data will be written to output.json in JSON format
 
         # Saving a list to a JSON file
         data = [1, 2, 3, 4, 5]
         save_to_json_file(data, "output.json")
-        # The contents of data  to output.json in JSON format
+        # The contents of data will be written to output.json in JSON format
     """
     with open(filename, "w") as file:
         json.dump(my_obj, file)
