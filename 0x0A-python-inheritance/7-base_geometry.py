01997bf43ae73b86ee860a6139a842044ab388c8
Eldad <eldadf456@gmail.com>
2024-02-05 21:33:12 -0800

    0x0A-python-inheritance

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -1,25 +1,41 @@
 #!/usr/bin/python3
-
-"""Defines a base geometry class BaseGeometry."""
+"""7-base_geometry module"""
 
 
 class BaseGeometry:
-    """Reprsent base geometry."""
-
+    """This is the  BaseGeometry class"""
     def area(self):
-        """Not yet implemented."""
+        """Public instance method -
+        that raises an Exception with the message
+        area() is not implemented
+        Raises:
+            Exception: area() is not implemented
+        """
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """Validate a parameter as an integer.
-        Args:
-            name (str): The name of the parameter.
-            value (int): The parameter to validate.
+        """Public instance method.
+        validates value.
         Raises:
-            TypeError: If value is not an integer.
-            ValueError: If value is <= 0.
+            TypeError: name must be a string
+            TypeError: name must not be an empty string
+            TypeError: "{} must be an integer".format(name)"
+            VlaueError: "{} must be greater than 0".format(name)"
         """
-        if type(value) != int:
+        if type(name) is not str:
+            raise TypeError("name must be a string")
+
+        if name is None or name is bool:
+            raise TypeError("name must be a string")
+
+        if len(name) == 0:
+            raise TypeError("name must not be an empty string")
+
+        if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
+
+        if value is None or value is bool:
+            raise TypeError("{} must be an integer".format(name))
+
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
