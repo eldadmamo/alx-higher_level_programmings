50de7a420569a95e10f32d1d6836fbf0ed1ab59b
Eldad <eldadf456@gmail.com>
2024-02-12 20:20:23 +0000

    0x0C-python-almost_a_circle

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+'''Class Square'''
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    '''Square class.'''
+
+    def __init__(self, size, x=0, y=0, id=None):
+        '''Constructor.'''
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        '''Returns string square.'''
+        return '[{}] ({}) {}/{} - {}'.\
+            format(type(self).__name__, self.id, self.x, self.y, self.width)
+
+    @property
+    def size(self):
+        '''square Size'''
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        self.width = value
+        self.height = value
+
+    def __update(self, id=None, size=None, x=None, y=None):
+        '''Internal method attributes via */**args.'''
+        if id is not None:
+            self.id = id
+        if size is not None:
+            self.size = size
+        if x is not None:
+            self.x = x
+        if y is not None:
+            self.y = y
+
+    def update(self, *args, **kwargs):
+        '''Update no-keyword & keyword args.'''
+        if args:
+            self.__update(*args)
+        elif kwargs:
+            self.__update(**kwargs)
+
+    def to_dictionary(self):
+        '''display dictionary value '''
+        return {"id": self.id, "size": self.width,
+                "x": self.x, "y": self.y}
