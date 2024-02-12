11c17c2aed7b94dddc87f779a9a1cd7caa7a7400
Eldad <eldadf456@gmail.com>
2024-02-12 19:28:36 +0000

    0x0C-python-almost_a_circle

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
-'''Base class.'''
+'''
+Base class.
+'''
 
 class Base:
- '''A base of object oriented programming'''
+ '''
+A base of object oriented programming
+'''
 
    __nb_objects = 0
 
@@ -11,5 +15,23 @@ class Base:
    if id is not None:
       self.id = id
    else:
-      Base.__nb_objects +=1
+      Base.__nb_objects += 1
       self.id = Base.__nb_objects
+
+
+if __name__ = "__main__";
+
+     b1 = Base()
+     print(b1.id)
+
+     b2 = Base()
+     print(b2.id)
+
+     b3 = Base()
+     print(b3.id)
+ 
+     b4 = Base()
+     print(b4.id)
+
+     b5 = Base()
+     print(b5.id)
